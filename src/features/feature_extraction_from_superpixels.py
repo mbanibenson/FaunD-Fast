0d16d7135b87ab89dc1b9773a572f9e7750b92cd
Benson Mbani <onyangombani@gmail.com>
2022-02-03 01:26:38 +0300

    Return to hand engineered

--- src/features/feature_extraction_from_superpixels.py
+++ src/features/feature_extraction_from_superpixels.py
@@ -126,9 +126,9 @@ def extract_hand_engineered_hog_support_set_feature_vectors(directory_containing
         
         support_set_labels.extend(labels_for_support_set_patches_in_this_subdirectory)
     
-    # support_set_patches_feature_vectors = extract_hand_engineered_hog_features_for_segmentation_patches(support_set_patches)
+    support_set_patches_feature_vectors = extract_hand_engineered_hog_features_for_segmentation_patches(support_set_patches)
     
-    support_set_patches_feature_vectors = extract_convnet_features_for_segmentation_patches_using_keras_applications(support_set_patches, resize_dimension=(224,224,3))
+    # support_set_patches_feature_vectors = extract_convnet_features_for_segmentation_patches_using_keras_applications(support_set_patches, resize_dimension=(224,224,3))
     
     
 
