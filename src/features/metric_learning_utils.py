651bfc7ae84e3bbdfcd5261a10c32e2161ecdc6d
Benson Mbani <onyangombani@gmail.com>
2022-02-02 14:43:07 +0300

    Use nca

--- src/features/metric_learning_utils.py
+++ src/features/metric_learning_utils.py
@@ -47,15 +47,15 @@ def embedd_segment_feature_vectors_using_supervised_pca(segmented_image_objects,
     optimization_results_object_for_finding_transformation_matrix, initial_transformation_matrix = run_optimization_to_obtain_prior_transformation_matrix(combined_feature_vectors, labels)
 
 
-#     nca = NeighborhoodComponentsAnalysis(n_components=2, init=initial_transformation_matrix)
+    nca = NeighborhoodComponentsAnalysis(n_components=2, init=initial_transformation_matrix)
 
-#     embedded_feature_vectors = nca.fit_transform(combined_feature_vectors, labels)
+    embedded_feature_vectors = nca.fit_transform(combined_feature_vectors, labels)
     
-    nca = LinearDiscriminantAnalysis(n_components=2)
+#     nca = LinearDiscriminantAnalysis(n_components=2)
     
-    nca.fit(combined_feature_vectors, labels)
+#     nca.fit(combined_feature_vectors, labels)
     
-    embedded_feature_vectors = nca.transform(combined_feature_vectors)
+#     embedded_feature_vectors = nca.transform(combined_feature_vectors)
     
     original_feature_vectors = combined_feature_vectors
     
