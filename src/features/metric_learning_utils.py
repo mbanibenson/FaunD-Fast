9bc6f0d60614f51dae030c2f0399c66697bf1371
Benson Mbani <onyangombani@gmail.com>
2022-02-04 12:13:32 +0300

    Change objective function in metric learning

--- src/features/metric_learning_utils.py
+++ src/features/metric_learning_utils.py
@@ -9,6 +9,8 @@ from scipy.optimize import minimize
 from numpy.random import default_rng
 from sklearn.decomposition import KernelPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import homogeneity_score
 
 rng = default_rng()
 
@@ -84,6 +86,67 @@ def find_transformation_matrix(A, x, b, design_matrix_shape):
     return frob_norm
 
 
+# def run_optimization_to_obtain_prior_transformation_matrix(original_feature_vectors, labels):
+#     '''
+#     Express prior on location of design matrix.
+    
+#     The optimization problem is to find a transformation matrix A such that x@A = b (x and b are row vectors)
+    
+#     Matrix A transforms original feature vectors (x) to the new (prior) embedding obtained from kernel pca
+    
+#     b is the (prior) embedding obtained from kernel pca. It captures similarity in labels
+    
+#     '''
+#     #We use kernel pca to give us a prior on where the coordinates would be in the 
+#     #embedding based on their similarity in labels
+#     X = np.array(labels).reshape(-1,1)
+
+#     pre_computed_kernel = cosine_similarity(X)
+
+#     transformer = KernelPCA(n_components=2, kernel='precomputed')
+    
+#     #These are the coordinates in the embedding purely based on label similarity
+#     X_transformed = transformer.fit_transform(pre_computed_kernel)
+    
+#     X_transformed = np.vstack([np.array([-1,0]) if int(label) == 0 else np.array([1,0]) for label in labels])
+
+    
+#     #We define the parameters of the optimization
+#     design_matrix_shape = (original_feature_vectors.shape[1], X_transformed.shape[1])
+
+#     #Reshape design matrix to a one-dim array for scipy.minimize
+#     A = rng.random(size=design_matrix_shape).ravel()
+
+#     x = original_feature_vectors
+
+#     b = X_transformed
+
+#     #Enforce constraint to prevent the trivial non zero solution
+#     cons = ({'type': 'ineq', 'fun': lambda v:  np.count_nonzero(v) - len(v)})
+    
+#     #cons = ({'type': 'ineq', 'fun': lambda v:  np.sum(v)})
+    
+    
+    
+#     #Loop indefinately until convergence
+#     while True:
+
+#         optimization_results_object_for_finding_transformation_matrix = minimize(fun = find_transformation_matrix, x0=A, args=(x, b, design_matrix_shape), options={'disp':True})
+        
+#         res_x = optimization_results_object_for_finding_transformation_matrix.x
+
+#         x_is_vector_of_zeros = np.allclose(res_x, np.zeros(res_x.shape))
+        
+#         if optimization_results_object_for_finding_transformation_matrix.success and not(x_is_vector_of_zeros):
+            
+#             break
+    
+#     #We get A as a one-dim vector, so we reshape it and transpose it as sklearn expects
+#     initial_transformation_matrix = optimization_results_object_for_finding_transformation_matrix.x.reshape(design_matrix_shape).T
+
+
+#     return optimization_results_object_for_finding_transformation_matrix, initial_transformation_matrix
+
 def run_optimization_to_obtain_prior_transformation_matrix(original_feature_vectors, labels):
     '''
     Express prior on location of design matrix.
@@ -95,41 +158,14 @@ def run_optimization_to_obtain_prior_transformation_matrix(original_feature_vect
     b is the (prior) embedding obtained from kernel pca. It captures similarity in labels
     
     '''
-    #We use kernel pca to give us a prior on where the coordinates would be in the 
-    #embedding based on their similarity in labels
-    X = np.array(labels).reshape(-1,1)
-
-    pre_computed_kernel = cosine_similarity(X)
-
-    transformer = KernelPCA(n_components=2, kernel='precomputed')
+    design_matrix_shape = (2, original_feature_vectors.shape[1])
     
-    #These are the coordinates in the embedding purely based on label similarity
-    X_transformed = transformer.fit_transform(pre_computed_kernel)
-    
-    X_transformed = np.vstack([np.array([-1,0]) if int(label) == 0 else np.array([1,0]) for label in labels])
+    initialization_matrix_A = rng.random(size=design_matrix_shape).ravel()
 
-    
-    #We define the parameters of the optimization
-    design_matrix_shape = (original_feature_vectors.shape[1], X_transformed.shape[1])
-
-    #Reshape design matrix to a one-dim array for scipy.minimize
-    A = rng.random(size=design_matrix_shape).ravel()
-
-    x = original_feature_vectors
-
-    b = X_transformed
-
-    #Enforce constraint to prevent the trivial non zero solution
-    cons = ({'type': 'ineq', 'fun': lambda v:  np.count_nonzero(v) - len(v)})
-    
-    #cons = ({'type': 'ineq', 'fun': lambda v:  np.sum(v)})
-    
-    
-    
     #Loop indefinately until convergence
     while True:
 
-        optimization_results_object_for_finding_transformation_matrix = minimize(fun = find_transformation_matrix, x0=A, args=(x, b, design_matrix_shape), options={'disp':True})
+        optimization_results_object_for_finding_transformation_matrix = minimize(fun = return_objective_function_for_finding_initial_transformation_matrix_for_nca, x0=initialization_matrix_A, args=(original_feature_vectors, labels), options={'disp':True})
         
         res_x = optimization_results_object_for_finding_transformation_matrix.x
 
@@ -140,7 +176,24 @@ def run_optimization_to_obtain_prior_transformation_matrix(original_feature_vect
             break
     
     #We get A as a one-dim vector, so we reshape it and transpose it as sklearn expects
-    initial_transformation_matrix = optimization_results_object_for_finding_transformation_matrix.x.reshape(design_matrix_shape).T
+    initial_transformation_matrix = optimization_results_object_for_finding_transformation_matrix.x.reshape(design_matrix_shape)
 
 
     return optimization_results_object_for_finding_transformation_matrix, initial_transformation_matrix
+
+
+def return_objective_function_for_finding_initial_transformation_matrix_for_nca(initialization_matrix_A, embeddings, labels):
+    '''
+    Find embedding matrix using clustering objective
+    
+    '''
+    
+    initialization_matrix_A = initialization_matrix_A.reshape(2, embeddings.shape[1])
+
+    transformed_embeddings = initialization_matrix_A @ embeddings.T
+    
+    clustering_labels = KMeans(n_clusters=2).fit(transformed_embeddings).labels_
+    
+    clustering_metric = homogeneity_score(clustering_labels, labels) - 0.5
+    
+    return clustering_metric
