544bcff2eea77e555e4306887968e5c3756cdc44
Benson Mbani <onyangombani@gmail.com>
2022-02-06 19:04:24 +0300

    increase iterations

--- src/features/metric_learning_utils.py
+++ src/features/metric_learning_utils.py
@@ -64,7 +64,7 @@ def embedd_segment_feature_vectors_using_supervised_pca(segmented_image_objects,
 
     #nca = NeighborhoodComponentsAnalysis(n_components=2, init=initial_transformation_matrix, verbose=2)
     
-    nca = NeighborhoodComponentsAnalysis(n_components=2, verbose=2)
+    nca = NeighborhoodComponentsAnalysis(n_components=2, verbose=2, max_iter=100)
 
     embedded_feature_vectors = nca.fit_transform(combined_feature_vectors, labels)
     
