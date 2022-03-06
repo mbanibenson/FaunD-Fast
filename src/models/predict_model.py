ba4a6284f25f0f2aadc3436ba066185c99cfb753
Benson Mbani <onyangombani@gmail.com>
2022-03-06 14:59:45 +0300

    Zoom the predictions by a factor of 3 as they are saved on disk

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -348,7 +348,7 @@ def save_patches_to_directory(directory_to_save_patches, patches, patch_names, p
     
     try:
     
-        [imsave(directory_to_save_patches / f'{patch_class}/{patch_name}.png', img_as_ubyte(patch)) for patch_class, patch_name, patch in zip(patch_class_labels, patch_names, patches)]
+        [imsave(directory_to_save_patches / f'{patch_class}/{patch_name}.png', zoom(img_as_ubyte(patch),(3,3,1))) for patch_class, patch_name, patch in zip(patch_class_labels, patch_names, patches)]
     
     except:
         
