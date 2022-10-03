eba2cb5d3b9a52be3254bcb803ff411f51d16556
mbanibenson <onyangombani@gmail.com>
2022-10-03 14:44:38 +0200

    Update class names after verification

--- src/update_detection_summary_table_after_verification.py
+++ src/update_detection_summary_table_after_verification.py
@@ -15,6 +15,17 @@ def remove_sort_id_from_patch_name(path_to_patch):
     return path_to_patch.stem.split('_', maxsplit=1)[1]
 
 
+def extract_morphotype_class_from_patch_name(path_to_patch):
+    '''
+    Infer the morphotype class from the file path of the patch. 
+    
+    The morphotype class is the directory within which the patch is located.
+    
+    '''
+    
+    return path_to_patch.parent.stem
+
+
 def update_detections_table(path_to_directory_with_cropped_patches, path_to_original_detections_summary_table, path_to_updated_detections_summary_table):
     '''
     Update table after detected patches have been verified and e.g. wrong detections deleted
@@ -23,9 +34,13 @@ def update_detections_table(path_to_directory_with_cropped_patches, path_to_orig
     
     original_megafauna_df = pd.read_csv(path_to_original_detections_summary_table)
     
-    current_patch_ids_after_verification = pd.DataFrame({'patch_id':[remove_sort_id_from_patch_name(path_to_patch) for path_to_patch in Path(path_to_directory_with_cropped_patches).rglob('*.png')]}) #Remove the sort id
+    patch_ids_after_verification = [remove_sort_id_from_patch_name(path_to_patch) for path_to_patch in Path(path_to_directory_with_cropped_patches).rglob('*.png')]
     
-    updated_megafauna_df = pd.merge(original_megafauna_df, current_patch_ids_after_verification, on='patch_id', how='inner')
+    class_names_after_verification = [extract_morphotype_class_from_patch_name(path_to_patch) for path_to_patch in Path(path_to_directory_with_cropped_patches).rglob('*.png')]
+    
+    verified_df = pd.DataFrame({'patch_id':patch_ids_after_verification, 'verified_class_name':class_names_after_verification})
+    
+    updated_megafauna_df = pd.merge(original_megafauna_df, verified_df, on='patch_id', how='inner')
     
     updated_megafauna_df.to_csv(path_to_updated_detections_summary_table, index=False)
     
